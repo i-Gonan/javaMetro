f9d898d9b01233a61a7d3b813c6b72cb475d8c45
i-Gonan <20231293@daejin.ac.kr>
2024-06-29 14:24:06 +0900

    build.gradle -> Windows 상에서 ./gradlew clean build로 jar 파일 생성 가능하도록 내용 수정 Main.java -> 이전에 추가했지만 Github 복구 과정에서 사라진 노선 단축어 복구 및 추가 readAPIKey.java -> 현행 방식으로는 jar 파일에서 코드를 못읽는 경우가 있어 읽는 방식 수정

--- src/main/java/readAPIKey.java
+++ src/main/java/readAPIKey.java
@@ -1,13 +1,16 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class readAPIKey {
 
     public static String getAPIKey(String testStnName) throws IOException {
         String metroAPI; // BufferedReader는 문자열로 읽어오기 때문에 문자열 변수 선언
+        //try (BufferedReader APIgetter = new BufferedReader(new FileReader("API.txt")))
 
-        try (BufferedReader APIgetter = new BufferedReader(new FileReader("src/main/resources/API.txt"))) {
+        try (InputStream inputStream = readAPIKey.class.getClassLoader().getResourceAsStream("API.txt");
+             BufferedReader APIgetter = new BufferedReader(new InputStreamReader(inputStream))) {
             // 파일을 읽어오기 위해서 BufferedReader 객체 생성
             metroAPI = APIgetter.readLine();
             int cnt = 0;
